3a77a7aac67180b87228c04e90ebab383913d16a
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-11-17 00:32:09 +0200

    Create navbar

--- src/landingPage/home.js
+++ src/landingPage/home.js
@@ -1,9 +1,20 @@
 import './home.css'
 
 export function Home() {
-  return <>
-    <nav className="navbar">
-      
+  return (
+  <>
+    <nav className="Nav navbar col-12">
+      <img className='p-2' src='/images/JS.png' alt='LOGO'></img>
+      <div className='SearchGroup d-flex col-5 gap-5'>
+        <div className='Fsearch gg-align-left'></div>
+        <input className='Tsearch' placeholder='SEARCH...'></input>
+        <div className='Bsearch gg-search'></div>
+      </div>
+      <div className='d-flex p-5 gap-5'>
+        <div className='notification gg-bell'></div>
+        <div className='UserProfile gg-profile'></div>  
+      </div>
     </nav>
   </>
+  );
 }
