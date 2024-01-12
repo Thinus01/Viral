3a8fbb5695121ee7e255f500f02b837428ab8baa
Thinus Van De Venter <thinusvdv2001@gmail.com>
2024-01-12 14:23:42 +0200

    Add filter animation

--- src/landingPage/home.js
+++ src/landingPage/home.js
@@ -1,20 +1,46 @@
 import './home.css'
+import './filterPopup.css'
 
 export function Home() {
+
+  const filterPopup = () => {
+    const filter = document.getElementById('filterPopup');
+
+    filter.classList.toggle('hide');
+  }
+
   return (
   <>
     <nav className="Nav navbar col-12">
-      <img className='p-2' src='/images/JS.png' alt='LOGO'></img>
-      <div className='SearchGroup d-flex col-5 gap-5'>
-        <div className='Fsearch gg-align-left'></div>
+      <div className='col-1'>
+         <img className='ViralLogo' src='/images/JS.png' alt='LOGO'></img>{/* placeholder logo */}
+      </div>
+      <div className='SearchGroup d-flex col-5'>
+        <div id='Fsearch' onClick={filterPopup}>
+          <div className='gg-align-left'></div>
+        </div>
         <input className='Tsearch' placeholder='SEARCH...'></input>
-        <div className='Bsearch gg-search'></div>
+        <div id='Bsearch'>
+          <div className='gg-search'></div>
+        </div>
       </div>
       <div className='d-flex p-5 gap-5'>
         <div className='notification gg-bell'></div>
         <div className='UserProfile gg-profile'></div>  
       </div>
     </nav>
+    <div id='filterPopup' className='hide'>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+        <p>filter</p>
+    </div>
   </>
   );
 }
