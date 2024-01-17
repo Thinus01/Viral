2c6f47cdb8a0bd6c82d268a842603344227e1dff
Thinus Van De Venter <thinusvdv2001@gmail.com>
2024-01-17 13:18:43 +0200

    Add notifycation and user popup

--- src/landingPage/home.js
+++ src/landingPage/home.js
@@ -1,12 +1,38 @@
 import './home.css'
 import './filterPopup.css'
+import './userPopup.css'
+import './notifyPopup.css'
 
 export function Home() {
 
   const filterPopup = () => {
+    const notifyPopup = document.getElementById('NotifyPopup');
+    const UserPopup = document.getElementById('UserPopup');
     const filter = document.getElementById('filterPopup');
 
     filter.classList.toggle('hide');
+    UserPopup.classList.add('hide');
+    notifyPopup.classList.add('hide');
+  }
+
+  const userPopup = () => {
+    const notifyPopup = document.getElementById('NotifyPopup');
+    const UserPopup = document.getElementById('UserPopup');
+    const filter = document.getElementById('filterPopup');
+
+    UserPopup.classList.toggle('hide');
+    filter.classList.add('hide');
+    notifyPopup.classList.add('hide');
+  }
+
+  const NotifyPopup = () => {
+    const notifyPopup = document.getElementById('NotifyPopup');
+    const UserPopup = document.getElementById('UserPopup');
+    const filter = document.getElementById('filterPopup');
+
+    notifyPopup.classList.toggle('hide');
+    filter.classList.add('hide');
+    UserPopup.classList.add('hide');
   }
 
   return (
@@ -25,10 +51,20 @@ export function Home() {
         </div>
       </div>
       <div className='d-flex p-5 gap-5'>
-        <div className='notification gg-bell'></div>
-        <div className='UserProfile gg-profile'></div>  
+        <div className='notification gg-bell' onClick={NotifyPopup}></div>
+        <div className='UserProfile gg-profile' onClick={userPopup}></div>  
       </div>
     </nav>
+    <div id='NotifyPopup' className='hide'>
+        <p>Nothing New yet!</p>
+    </div>
+    <div id='UserPopup' className='hide'>
+        <p>Profile</p>
+        <p>Setting</p>
+        <p>L&A</p>
+        <p>Log Out</p>
+        <p>Friends</p>
+    </div>
     <div id='filterPopup' className='hide'>
         <p>filter</p>
         <p>filter</p>
